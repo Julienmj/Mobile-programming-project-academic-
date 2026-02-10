41a448b7fbe8d74b02b0e54594828e6d4ca310ca
Mugisha Julien <julienmugisha1@gmail.com>
2026-02-10 15:24:17 +0200

    fix: Add missing ViewGroup import to resolve build error

--- app/src/main/java/com/example/quiz_3/StudentListActivity.java
+++ app/src/main/java/com/example/quiz_3/StudentListActivity.java
@@ -3,6 +3,7 @@ package com.example.quiz_3;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
